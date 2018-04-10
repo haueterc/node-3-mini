4a90bdc4b1c3d563b253f73a41927de6b885a1ec
Chandler Haueter <chandlerhaueter@Misc.local>
2018-04-10 13:46:19 -0600

    code progress

--- server/index.js
+++ server/index.js
@@ -1,11 +1,36 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mc = require( `./controllers/messages_controller` );
+const session = require('express-session');
+const createInitialSession = './middlewares/session';
+const filter = './middlewares/filter';
+require('dotenv').config();
 
 const app = express();
 
 app.use( bodyParser.json() );
+
+// How to serve up your app once you're live
 app.use( express.static( `${__dirname}/../build` ) );
+
+app.use(session({
+    resave: false,
+    saveUinitialized: true,
+    secret: process.env.SESSION_SECRET,
+    cookie: {
+        maxAge: 10000
+    }
+}))
+
+app.use((req, res, next) => createInitialSession(req, res, next))
+
+app.use((req, res, next) => {
+    if (req.method === 'POST' || req.method === 'PUT') {
+        filter(req, res, next)
+    } else {
+        next()
+    }
+})
 
 app.post( "/api/messages", mc.create );
 app.get( "/api/messages", mc.read );
